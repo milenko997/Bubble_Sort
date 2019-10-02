338f251382ff64226aa67e6fa7855756732ec2d2
milenko997 <48036062+milenko997@users.noreply.github.com>
2019-10-02 10:33:55 +0200

    commit

--- bubble_sort.go
+++ bubble_sort.go
@@ -3,20 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	a := []int{1, 2, 3, 5, 4, 6}
+	a := []int{2, 1, 3, 4, 5, 6}
 	fmt.Println("Unsorted array: ", a)
 	bubbleSort(a)
 	fmt.Println("Sorted array: ", a)
 }
 func bubbleSort(a []int) {
-	sorted := false
 	end := len(a) - 1
 	for {
-		if sorted {
-			break
-		}
-		fmt.Println(end)
 		swapped := false
+		fmt.Println(end)
 		for i := 0; i < len(a)-1; i++ {
 			//fmt.Println(end, i)
 			if a[i] > a[i+1] {
@@ -25,7 +21,7 @@ func bubbleSort(a []int) {
 			}
 		}
 		if !swapped {
-			sorted = true
+			break
 		}
 	}
 }
